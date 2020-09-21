2a5451175279a685f0799d45667dbe4a4816453d
Emil Martens <emil.martens@gmail.com>
2020-09-21 20:34:13 +0200

    cleanup

--- src/equations.py
+++ src/equations.py
@@ -5,8 +5,14 @@ Created on Mon Mar 16 21:00:43 2020
 @author: user_id
 """
 from sympy import symbols
-from sympy.physics.mechanics import *
-from sympy.printing.pycode import NumPyPrinter, pycode
+from sympy.physics.mechanics import (
+    dynamicsymbols,
+    ReferenceFrame,
+    Point,
+    Particle,
+    KanesMethod
+)
+# from sympy.printing.pycode import NumPyPrinter, pycode
 n = 5
 
 q = dynamicsymbols('q:' + str(n + 1))  # Generalized coordinates
@@ -14,50 +20,61 @@ u = dynamicsymbols('u:' + str(n + 1))  # Generalized speeds
 f = dynamicsymbols('f')                # Force applied to the cart
 
 m = symbols('m:' + str(n + 1))         # Mass of each bob
-l = symbols('l:' + str(n))             # Length of each link
+length = symbols('l:' + str(n))        # Length of each link
 g, t = symbols('g t')                  # Gravity and time
 
-I = ReferenceFrame('I')                # Inertial reference frame
-O = Point('O')                         # Origin point
-O.set_vel(I, 0)                        # Origin's velocity is zero
+ref_frame = ReferenceFrame('I')     # Inertial reference frame
+origin = Point('O')                 # Origin point
+origin.set_vel(ref_frame, 0)        # Origin's velocity is zero
 
 
-
-P0 = Point('P0')                       # Hinge point of top link
-P0.set_pos(O, q[0] * I.x)              # Set the position of P0
-P0.set_vel(I, u[0] * I.x)              # Set the velocity of P0
-Pa0 = Particle('Pa0', P0, m[0])        # Define a particle at P0
-
+P0 = Point('P0')                            # Hinge point of top link
+P0.set_pos(origin, q[0] * ref_frame.x)      # Set the position of P0
+P0.set_vel(ref_frame, u[0] * ref_frame.x)   # Set the velocity of P0
+Pa0 = Particle('Pa0', P0, m[0])             # Define a particle at P0
 
 
-
-frames = [I]                              # List to hold the n + 1 frames
+# List to hold the n + 1 frames
+frames = [ref_frame]
 points = [P0]                             # List to hold the n + 1 points
 particles = [Pa0]                         # List to hold the n + 1 particles
-forces = [(P0, f * I.x - m[0] * g * I.y)] # List to hold the n + 1 applied forces, including the input force, f
+
+# List to hold the n + 1 applied forces, including the input force, f
+forces = [(P0, f * ref_frame.x - m[0] * g *
+           ref_frame.y - 0.01 * q[0] * ref_frame.x)]
 kindiffs = [q[0].diff(t) - u[0]]          # List to hold kinematic ODE's
 
 for i in range(n):
-    Bi = I.orientnew('B' + str(i), 'Axis', [q[i + 1], I.z])   # Create a new frame
-    Bi.set_ang_vel(I, u[i + 1] * I.z)                         # Set angular velocity
-    frames.append(Bi)                                         # Add it to the frames list
+    # Create a new frame
+    Bi = ref_frame.orientnew('B' + str(i), 'Axis', [q[i + 1], ref_frame.z])
+    # Set angular velocity
+    Bi.set_ang_vel(ref_frame, u[i + 1] * ref_frame.z)
+    # Add it to the frames list
+    frames.append(Bi)
 
-    Pi = points[-1].locatenew('P' + str(i + 1), l[i] * Bi.x)  # Create a new point
-    Pi.v2pt_theory(points[-1], I, Bi)                         # Set the velocity
-    points.append(Pi)                                         # Add it to the points list
+    Pi = points[-1].locatenew('P' + str(i + 1), length[i]
+                              * Bi.x)  # Create a new point
+    # Set the velocity
+    Pi.v2pt_theory(points[-1], ref_frame, Bi)
+    # Add it to the points list
+    points.append(Pi)
 
-    Pai = Particle('Pa' + str(i + 1), Pi, m[i + 1])           # Create a new particle
-    particles.append(Pai)                                     # Add it to the particles list
+    # Create a new particle
+    Pai = Particle('Pa' + str(i + 1), Pi, m[i + 1])
+    # Add it to the particles list
+    particles.append(Pai)
 
-    forces.append((Pi, -m[i + 1] * g * I.y - 0.001* u[i + 1] * Bi.y))# Set the force applied at the point
-    forces.append((points[-2],   0.001* u[i + 1] * Bi.y))# Set the force applied at the point
+    # Set the force applied at the point
+    forces.append((Pi, -m[i + 1] * g * ref_frame.y - 0.001 * u[i + 1] * Bi.y))
 
-    kindiffs.append(q[i + 1].diff(t) - u[i + 1])              # Define the kinematic ODE:  dq_i / dt - u_i = 0
+    # Set the force applied at the point
+    forces.append((points[-2],   0.001 * u[i + 1] * Bi.y))
 
-
-
-
-kane = KanesMethod(I, q_ind=q, u_ind=u, kd_eqs=kindiffs) # Initialize the object
-fr, frstar = kane.kanes_equations(particles, forces)     # Generate EoM's fr + frstar = 0
+    # Define the kinematic ODE:  dq_i / dt - u_i = 0
+    kindiffs.append(q[i + 1].diff(t) - u[i + 1])
 
 
+# Initialize the object
+kane = KanesMethod(ref_frame, q_ind=q, u_ind=u, kd_eqs=kindiffs)
+# Generate EoM's fr + frstar = 0
+fr, frstar = kane.kanes_equations(particles, forces)
--- src/simulate.py
+++ src/simulate.py
@@ -5,33 +5,43 @@ Created on Mon Mar 16 21:29:09 2020
 @author: user_id
 """
 
-from equations import *
+from equations import n, m, g, length, kane, q, u, f
 from sympy import Dummy, lambdify
-from numpy import array, hstack, zeros, linspace, pi, ones
+from numpy import array, hstack, linspace, pi, ones
 from numpy.linalg import solve
 from scipy.integrate import odeint
-from matplotlib.pyplot import plot, xlabel, legend, show
 from animate import animate_pendulum
+from matplotlib import pyplot as plt
 
-
-arm_length = 1. / n                          # The maximum length of the pendulum is 1 meter
-bob_mass = 0.01 / n                          # The maximum mass of the bobs is 10 grams
-parameters = [g, m[0]]                       # Parameter definitions starting with gravity and the first bob
-parameter_vals = [9.81, 0.01 / n]            # Numerical values for the first two
+# The maximum length of the pendulum is 1 meter
+arm_length = 1. / n
+# The maximum mass of the bobs is 10 grams
+bob_mass = 0.01 / n
+# Parameter definitions starting with gravity and the first bob
+parameters = [g, m[0]]
+# Numerical values for the first two
+parameter_vals = [9.81, 0.01 / n]
 for i in range(n):                           # Then each mass and length
-    parameters += [l[i], m[i + 1]]
+    parameters += [length[i], m[i + 1]]
     parameter_vals += [arm_length, bob_mass]
 
-dynamic = q + u                                                # Make a list of the states
-dynamic.append(f)                                              # Add the input force
-dummy_symbols = [Dummy() for i in dynamic]                     # Create a dummy symbol for each variable
+# Make a list of the states
+dynamic = q + u
+# Add the input force
+dynamic.append(f)
+# Create a dummy symbol for each variable
+dummy_symbols = [Dummy() for i in dynamic]
 dummy_dict = dict(zip(dynamic, dummy_symbols))
-kindiff_dict = kane.kindiffdict()                              # Get the solved kinematical differential equations
-M = kane.mass_matrix_full.subs(kindiff_dict).subs(dummy_dict)  # Substitute into the mass matrix
-F = kane.forcing_full.subs(kindiff_dict).subs(dummy_dict)      # Substitute into the forcing vector
-M_func = lambdify(dummy_symbols + parameters, M)               # Create a callable function to evaluate the mass matrix
-F_func = lambdify(dummy_symbols + parameters, F)               # Create a callable function to evaluate the forcing vector
-
+# Get the solved kinematical differential equations
+kindiff_dict = kane.kindiffdict()
+M = kane.mass_matrix_full.subs(kindiff_dict).subs(
+    dummy_dict)  # Substitute into the mass matrix
+F = kane.forcing_full.subs(kindiff_dict).subs(
+    dummy_dict)      # Substitute into the forcing vector
+# Create a callable function to evaluate the mass matrix
+M_func = lambdify(dummy_symbols + parameters, M)
+# Create a callable function to evaluate the forcing vector
+F_func = lambdify(dummy_symbols + parameters, F)
 
 
 def right_hand_side(x, t, args):
@@ -54,15 +64,18 @@ def right_hand_side(x, t, args):
     """
     u = 0.0                              # The input force is always zero
     arguments = hstack((x, u, args))     # States, input, and parameters
-    dx = array(solve(M_func(*arguments), # Solving for the derivatives
-        F_func(*arguments))).T[0]
+    dx = array(solve(M_func(*arguments),  # Solving for the derivatives
+                     F_func(*arguments))).T[0]
 
     return dx
 
 
-x0 = hstack(( 0, pi / 2 * ones(len(q) - 1), 1e-3 * ones(len(u)) )) # Initial conditions, q and u
-t = linspace(0, 10, 1000)                                          # Time vector
-y = odeint(right_hand_side, x0, t, args=(parameter_vals,))         # Actual integration
+# Initial conditions, q and u
+x0 = hstack((0, pi / 2 * ones(len(q) - 1), 1e-3 * ones(len(u))))
+# Time vector
+t = linspace(0, 10, 1000)
+y = odeint(right_hand_side, x0, t, args=(
+    parameter_vals,))         # Actual integration
 
 
 # lines = plot(t, y[:, :y.shape[1] // 2])
@@ -71,7 +84,6 @@ y = odeint(right_hand_side, x0, t, args=(parameter_vals,))         # Actual inte
 
 
 fig, anim = animate_pendulum(t, y, arm_length)
-
+plt.show()
 
 # return fig, anim
-
