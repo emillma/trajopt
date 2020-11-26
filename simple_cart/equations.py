8d8e6294c1d224e6e25a35914c041222db7d5edf
Emil Martens <emil.martens@gmail.com>
2020-11-26 16:06:00 +0100

    .

--- simple_cart/equations.py
+++ simple_cart/equations.py
@@ -10,6 +10,7 @@ import sympy as sp
 import numpy as np
 from scipy.integrate import odeint
 import control
+from sympy.printing.pycode import NumPyPrinter
 from sympy.physics.mechanics import (
     dynamicsymbols,
     ReferenceFrame,
@@ -18,9 +19,10 @@ from sympy.physics.mechanics import (
     KanesMethod
 )
 # from sympy.printing.pycode import NumPyPrinter, pycode
-q = dynamicsymbols('q:1')  # Generalized coordinates
-u = dynamicsymbols('u:1')  # Generalized speeds
-f = dynamicsymbols('f')                # Force applied to the cart
+coordinates = dynamicsymbols('q:1')  # Generalized coordinates
+speeds = dynamicsymbols('u:1')  # Generalized speeds
+# Force applied to the cart
+cart_thrust = dynamicsymbols('thrust')
 
 m = sp.symbols('m:1')         # Mass of each bob
 g, t = sp.symbols('g t')                  # Gravity and time
@@ -31,8 +33,8 @@ origin.set_vel(ref_frame, 0)        # Origin's velocity is zero
 
 
 P0 = Point('P0')                            # Hinge point of top link
-P0.set_pos(origin, q[0] * ref_frame.x)      # Set the position of P0
-P0.set_vel(ref_frame, u[0] * ref_frame.x)   # Set the velocity of P0
+P0.set_pos(origin, coordinates[0] * ref_frame.x)      # Set the position of P0
+P0.set_vel(ref_frame, speeds[0] * ref_frame.x)   # Set the velocity of P0
 Pa0 = Particle('Pa0', P0, m[0])             # Define a particle at P0
 
 
@@ -42,80 +44,58 @@ points = [P0]                             # List to hold the n + 1 points
 particles = [Pa0]                         # List to hold the n + 1 particles
 
 # List to hold the n + 1 applied forces, including the input force, f
-forces = [(P0, f * ref_frame.x - m[0] * g *
+forces = [(P0, cart_thrust * ref_frame.x - m[0] * g *
            ref_frame.y)]
-kindiffs = [q[0].diff(t) - u[0]]          # List to hold kinematic ODE's
+# List to hold kinematic ODE's
+kindiffs = [coordinates[0].diff(t) - speeds[0]]
 
 
 # Initialize the object
-kane = KanesMethod(ref_frame, q_ind=q, u_ind=u, kd_eqs=kindiffs)
+kane = KanesMethod(ref_frame, q_ind=coordinates, u_ind=speeds, kd_eqs=kindiffs)
 # Generate EoM's fr + frstar = 0
 fr, frstar = kane.kanes_equations(particles, forces)
 
-dynamic = q + u + [f]
-# Add the input force
-# Create a dummy symbol for each variable
+dynamic = coordinates + speeds + [cart_thrust]
+
 dummy_symbols = [sp.Dummy() for i in dynamic]
 dummy_dict = dict(zip(dynamic, dummy_symbols))
 
 # Get the solved kinematical differential equations
 kindiff_dict = kane.kindiffdict()
 
-M = kane.mass_matrix_full.subs(kindiff_dict).subs(
-    dummy_dict)  # Substitute into the mass matrix
+M = kane.mass_matrix_full.subs(kindiff_dict)
 
-F = kane.forcing_full.subs(kindiff_dict).subs(
-    dummy_dict)
+F = kane.forcing_full.subs(kindiff_dict)
 
 parameters = [g, m[0]]
 parameter_vals = [9.81, 1]
-M_func = sp.lambdify(dummy_symbols + parameters, M)
-F_func = sp.lambdify(dummy_symbols + parameters, F)
-
-M, MA, MB, U = kane.linearize()
-# sub in the equilibrium point and the parameters
 parameter_dict = dict(zip(parameters, parameter_vals))
-M = M.subs(parameter_dict)
-MA = MA.subs(parameter_dict)
-MB = MB.subs(parameter_dict)
 
-M_inv = M.inv()
-A = M_inv * MA
-B = M_inv * MB
+f_dyn = M.inv() * F
+f_dyn = f_dyn.subs(parameter_dict)
 
-K = control.acker(A, B, [-1, -1])
+dynamic = coordinates + speeds + [cart_thrust]
+dummy_symbols = [sp.Dummy() for i in dynamic]
+dummy_dict = dict(zip(dynamic, dummy_symbols))
+f_dyn_lambda = sp.lambdify(dummy_symbols, f_dyn.subs(dummy_dict))
 
+n = 11
+timestep = sp.symbols('h')
+timestep = 0.1
+nlp_x = [sp.symbols(f'x_:{n}\,:2')[i:i+2] for i in range(0, n*2, 2)]
+nlp_u = sp.symbols(f'u_:{n}')
+constraints = []
+for i in range(n-1):
+    x_kp0 = sp.Matrix(nlp_x[i])
+    x_kp1 = sp.Matrix(nlp_x[i+1])
 
-def right_hand_side(x, t, args):
-    """Returns the derivatives of the states.
+    u_kp0 = nlp_u[i]
+    u_kp1 = nlp_u[i+1]
 
-    Parameters
-    ----------
-    x : ndarray, shape(2 * (n + 1))
-        The current state vector.
-    t : float
-        The current time.
-    args : ndarray
-        The constants.
+    f_kp0 = f_dyn.subs(dict(zip(dynamic, [*x_kp0, u_kp0])))
+    f_kp1 = f_dyn.subs(dict(zip(dynamic, [*x_kp1, u_kp1])))
+    constraints.extend([*(
+        timestep * (f_kp0 + f_kp1) / 2. - (x_kp1 - x_kp0))])
 
-    Returns
-    -------
-    dx : ndarray, shape(2 * (n + 1))
-        The derivative of the state.
-
-    """
-    u = np.array(-K@(x-np.array([1, 0]))
-                 ).squeeze()                            # The input force is always zero
-    arguments = np.hstack((x, u, args))     # States, input, and parameters
-    dx = np.array(np.linalg.solve(M_func(*arguments),  # Solving for the derivatives
-                                  F_func(*arguments))).T[0]
-
-    return dx
-
-
-kane.linearize()
-x0 = np.hstack((0, 0))
-# Time vector
-t = np.linspace(0, 10, 1000)
-y = odeint(right_hand_side, x0, t, args=(
-    parameter_vals,))         # Actual integration
+args = [nlp_x, nlp_u]
+lam = sp.lambdify(args, constraints)
